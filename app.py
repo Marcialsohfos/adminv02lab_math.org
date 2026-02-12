aae085933ac0c62379f02e6f8cbf22e7c4d5c160
Marcialsohfos <marcialteda@gmail.com>
2026-02-12 11:11:52 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -571,6 +571,5 @@ with app.app_context():
         print(f"❌ Erreur lors de l'initialisation: {str(e)}")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
